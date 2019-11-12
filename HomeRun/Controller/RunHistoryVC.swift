064cac0ae0bc8e1ef232d20dd279d20831d9c6d5
nazar petruk <nazarpetruk15@gmail.com>
2019-11-12 17:45:03 +0100

    - history of runs screen made

--- HomeRun/Controller/RunHistoryVC.swift
+++ HomeRun/Controller/RunHistoryVC.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class RunHistoryVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
 
 
