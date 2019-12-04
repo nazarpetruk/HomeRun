e7888c34b63fc9d65265caa2479d2a578a05ca39
nazar petruk <nazarpetruk15@gmail.com>
2019-12-04 16:30:42 +0100

    - last run view added

--- HomeRun/Controller/BeginRunVC.swift
+++ HomeRun/Controller/BeginRunVC.swift
@@ -14,6 +14,11 @@ class BeginRunVC: LocationVC {
 //MARK: IBOutlet
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var startRunBtn: UIButton!
+    @IBOutlet weak var closeLastRunBtn: UIButton!
+    @IBOutlet weak var paceLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
+    @IBOutlet weak var timeRunLbl: UILabel!
+    @IBOutlet weak var lastRunView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +27,8 @@ class BeginRunVC: LocationVC {
         
         mapView.layer.cornerRadius = 10
         mapView.layer.masksToBounds = true
+        lastRunView.layer.cornerRadius = 10
+        lastRunView.layer.masksToBounds = true
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -32,6 +39,7 @@ class BeginRunVC: LocationVC {
     override func viewWillAppear(_ animated: Bool) {
         manager?.delegate = self
         manager?.startUpdatingLocation()
+        getLastRun()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -40,10 +48,29 @@ class BeginRunVC: LocationVC {
     
     
 //MARK: IBAction
+
     @IBAction func locationBtnPressed(_ sender: Any) {
     }
     
+    @IBAction func cancelLastRunBtnPressed(_ sender: Any) {
+        lastRunView.isHidden = true
+        closeLastRunBtn.isHidden = true
+    }
     
+    
+//MARK: Funcs
+    func getLastRun(){
+        guard let lastRun = Run.getRuns()?.first else {
+            lastRunView.isHidden = true
+            closeLastRunBtn.isHidden = true
+            return
+        }
+        lastRunView.isHidden = false
+        closeLastRunBtn.isHidden = false
+        paceLbl.text = lastRun.pace.intoStringFormatter()
+        distanceLbl.text = "\(lastRun.distance.mIntoKm(decimalNums: 2)) /km"
+        timeRunLbl.text = lastRun.duration.intoStringFormatter()
+    }
 }
 
 //MARK: Extensions
